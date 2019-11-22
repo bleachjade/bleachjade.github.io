db4becb0498e567a0438059a63c462f0afc96d56
bleachjade <bleachjade@hotmail.com>
2019-11-22 10:54:44 +0700

    add more information section and fix an header animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const hero = document.querySelector(".hero");
+const container = document.querySelector(".container");
 const slider = document.querySelector(".slider");
 const logo = document.querySelector("#logo");
 const hamburger = document.querySelector(".hamburger");
@@ -6,9 +6,9 @@ const headline = document.querySelector(".headline");
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+tl.fromTo(container, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
   .fromTo(
-    hero,
+    container,
     1.2,
     { width: "100%" },
     { width: "80%", ease: Power2.easeInOut }
@@ -23,3 +23,15 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
   .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(hamburger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
+
+  new fullpage('#fullpage', {
+    //options here
+      autoScrolling:true,
+      sectionsColor: ['black', 'green', 'black', 'white', 'black'],
+      navigation: true,
+      onLeave: (origin,destination,direction)=> {
+          const section = destination.item;
+          console.log(section);
+          TweenMax.fromTo(section, 1, {opacity: 0}, {opacity: 1},'+=1000');
+      }
+  });
