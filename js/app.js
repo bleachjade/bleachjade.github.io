57fa8312c9cabcae644d08bc8142d21642b8d2cb
bleachjade <bleachjade@hotmail.com>
2020-04-20 15:55:51 +0700

    add mobile frly, animation bugs

--- js/app.js
+++ js/app.js
@@ -3,27 +3,47 @@ const slider = document.querySelector(".slider");
 const logo = document.querySelector("#logo");
 const hamburger = document.querySelector(".hamburger");
 const headline = document.querySelector(".headline");
+const headline_content = document.querySelector(".headline-content");
 
 const tl = new TimelineMax();
 
+function IMGcontainer(x) {
+  if (x.matches) { // If media query matches
+    tl.fromTo(container,
+      1,
+      { width: "50%" },
+      { width: "100%", ease: Power2.easeInOut },
+      { height: "60%" },
+      { height: "80%", ease: Power2.easeInOut });
+  } else {
+   tl.fromTo(container,
+    1.5,
+    { width: "70%" },
+    { width: "50%", ease: Power2.easeInOut });
+  }
+};
+var x = window.matchMedia("(max-width: 600px)")
+IMGcontainer(x) // Call listener function at run time
+x.addListener(IMGcontainer)
+
 tl.fromTo(container, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-  .fromTo(
-    container,
-    1.2,
-    { width: "80%" },
-    { width: "50%", ease: Power2.easeInOut }
-  )
+  // .fromTo(container,
+  //     1.5,
+  //     { width: "70%" },
+  //     { width: "50%", ease: Power2.easeInOut })
   .fromTo(
     slider,
     1.5,
-    { x: "-100%" },
-    { x: "0%", ease: Power2.easeInOut },
+    { y: "-100%" },
+    { y: "0%", ease: Power2.easeInOut },
     "-=1.2"
   )
   .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
   .fromTo(hamburger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
-  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(headline_content, 0.5, { opacity: 0, x: -100}, { opacity: 1, x: 0}, "-=0.5");
 
+  //pages navigation test//
   new fullpage('#fullpage', {
     //options here
       autoScrolling:true,
